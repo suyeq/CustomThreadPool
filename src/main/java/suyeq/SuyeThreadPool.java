85f1fcd39a9737806593bb4a387ce4c8e7409377
suyeq <473721601@qq.com>
2019-03-09 11:14:02 +0800

    2

--- src/main/java/suyeq/SuyeThreadPool.java
+++ src/main/java/suyeq/SuyeThreadPool.java
@@ -7,6 +7,9 @@ import java.util.HashSet;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created with IntelliJ IDEA.
@@ -40,6 +43,10 @@ public class SuyeThreadPool implements Executor {
 
     private final ThreadRepository threadRepository;
 
+    private final Lock mainLock=new ReentrantLock();
+
+    private final Condition mainCondition=mainLock.newCondition();
+
     public SuyeThreadPool(){
         this.bestPoolThreadSize=Runtime.getRuntime().availableProcessors();
         this.theMostPoolThreadSize=bestPoolThreadSize;
@@ -62,28 +69,39 @@ public class SuyeThreadPool implements Executor {
 
     }
 
-    public void addWorkThread(Runnable firstTask){
+    public boolean addWorkThread(Runnable firstTask){
         retry:
         for (;;){
             /**
              * 如果线程池的状态大于Stop或者任务队列为null
              * 又或者线程第一个任务为null，那么就拒绝创建新的线程
-             * 又或者最大工作者数量大于
+             * 又或者工作者线程数量大于最大工作者线程数量
              */
             int poolState=suyeThreadPoolState.getPoolState();
             int poolThreadSize=suyeThreadPoolState.getWorkThreadSize();
             if (taskQueue.isEmpty() || firstTask==null){
-               return;
+               return false;
             }else if (poolState>=suyeThreadPoolState.StopState()){
-                return;
+                return false;
             }else if(poolThreadSize>=theMostPoolThreadSize){
-                return;
+                return false;
             }else {
                 break retry;
             }
         }
         WorkThread workThread=new WorkThread(firstTask,threadRepository,this);
+        if (workThread!=null){
+            int poolThreadSize=suyeThreadPoolState.getWorkThreadSize();
+            int poolState=suyeThreadPoolState.getPoolState();
+            final Thread thread=workThread.getThread();
+            this.mainLock.lock();
+            if (thread.isAlive()){
+                throw new IllegalStateException();
+            }
+            workThreadSet.add(workThread);
 
+        }
+        return true;
     }
 
     public Runnable getTask(){
